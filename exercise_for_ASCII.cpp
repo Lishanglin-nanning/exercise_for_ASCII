ba845c274a2cf0d5f704af6b22d637e6eb9a7b0f
LiShanglin <LiShanglin@DESKTOP-0ACGH0P>
2021-01-25 23:41:35 +0800

    添加项目文件。

---
+++ exercise_for_ASCII.cpp
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+int main(void)
+{
+	int input;
+	printf("Enter a value of char int ASCII:");
+	scanf_s("%d", &input);
+	printf("You input value is %d,and char is %c\n", input, input);
+	return 0;
+
+}
